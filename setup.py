ebb33997baeb1746e08b2be87118813e33114ab6
name <you@example.com>
2018-08-07 20:54:35 +0300

    All's done NOW. Resolved more pylint and flake8 conflicts.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 """Setup file of the client side of the project."""
 
-
 from setuptools import setup
 from setuptools import find_packages
 
 setup(name='Supercalifragilisticexpialidocius Monitoring System Client',
       version='1.0',
       description='Application which monitors metrics on PC - Client Side',
-      url = 'https://github.com/BabyCakes13/Supercalifragilisticexpialidocius-Monitoring-System-Client.git',
+      url='https://github.com/BabyCakes13/'
+          'Supercalifragilisticexpialidocius-Monitoring-System-Client.git',
       author='Maria Minerva Vonica',
       author_email='maria.vonica98@gmail.com',
       packages=find_packages())
